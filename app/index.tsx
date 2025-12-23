719946b22d5f38299ab9dfc5f691558e753b459d
namecoder1 <bartolomei.private@gmail.com>
2025-12-23 15:33:07 +0100

    added expo-icons

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,12 @@
-import { Text, View } from "react-native";
+import { Text, useColorScheme, View } from "react-native";
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 export default function Index() {
+  const colorScheme = useColorScheme()
+
   return (
-    <View className="flex-1 justify-center items-center bg-background">
+    <View className="flex-1 justify-center bg-background flex-col items-center gap-4">
+      <AntDesign name="code" size={32} color={colorScheme === 'dark' ? 'white' : 'black'} />
       <Text className="text-center text-2xl px-6 text-muted-foreground">Try using some Tailwind Classes in the markup like <Text className="font-semibold text-foreground">'bg-red-500 flex items-center'</Text>.</Text>
     </View>
   );
